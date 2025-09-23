6d4a15b1ec82efe9d9cf2acda2ae193618fe4469
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-09-23 19:07:07 -0400

    Add index and new message routes along with messages array

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,20 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => res.send("Home page"));
+indexRouter.get("/new", (req, res) => res.send("New Message Form"));
 
 module.exports = indexRouter;
