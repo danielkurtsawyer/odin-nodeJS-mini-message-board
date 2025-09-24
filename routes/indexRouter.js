bc5de529b4a8ad8293ceae086cd436f9ced188cd
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-09-24 17:02:58 -0400

    Add message details page and routing

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,13 +7,17 @@ const messages = [
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
+    id: 1,
   },
   {
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
+    id: 2,
   },
 ];
+
+let id = 3;
 
 indexRouter.get("/", (req, res) =>
   res.render("index", {
@@ -27,11 +31,27 @@ indexRouter.get("/new", (req, res) =>
   })
 );
 
+indexRouter.get("/message/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const message = messages.find((message) => message.id === Number(id));
+  console.log(message);
+
+  if (!message) {
+    res.status(404).send("Message not found");
+    return;
+  }
+  res.render("messagePage", {
+    title: `Message ${id} Details`,
+    message: message,
+  });
+});
+
 indexRouter.post("/new", (req, res) => {
   const user = req.body.name;
   const text = req.body.text;
   const added = new Date();
-  messages.push({ text: text, user: user, added: added });
+  messages.push({ text: text, user: user, added: added, id: id++ });
   res.redirect("/");
 });
 
